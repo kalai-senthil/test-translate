bca1c00845a2df3f0a5d0a458116f95193c7a887
Kalaiarasan Senthil <70711693+kalai-senthil@users.noreply.github.com>
2023-11-03 09:02:17 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector("button")
-const inp = document.querySelector("input")
-const card = document.querySelector(".card")
-btn.addEventListener("click", (e) => {
-    const newNode = document.createElement("p")
-    newNode.append(document.createTextNode(inp.value))
-    card.append(newNode)
-})
